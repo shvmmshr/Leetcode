546a8a4173bd0aeef64d49d7ebc6b192a382caa9
Shivam Mishra <shivammishra15092003@gmail.com>
2024-11-15 00:08:07 +0530

    Time: 31 ms (79.40%) | Memory: 87.2 MB (72.13%) - LeetSync

--- 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    bool canDistribute(int n, vector<int>& quantities, int maxPerStore) {
-        int storesNeeded = 0;
-        for (int quantity : quantities) {
-            storesNeeded += (quantity + maxPerStore - 1) / maxPerStore;
-            if (storesNeeded > n) return false;
+    bool canDist(int n, vector<int>& quantities, int maxPer) {
+        int stores = 0;
+        for (int q : quantities) {
+            stores += (q + maxPer - 1) / maxPer;
+            if (stores > n) return false;
         }
-        return storesNeeded <= n;
+        return stores <= n;
     }
+    
     int minimizedMaximum(int n, vector<int>& quantities) {
         int left = 1, right = *max_element(quantities.begin(), quantities.end());
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (canDistribute(n, quantities, mid)) {
+            if (canDist(n, quantities, mid)) {
                 right = mid;
             } else {
                 left = mid + 1;
